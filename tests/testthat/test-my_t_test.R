a2f0614eb135133c1fdaca09207bf7342c05c8b0
Mark Lamin <mark.lamin@gmail.com>
2021-06-03 13:20:45 -0700

    Tests for t test

---
+++ tests/testthat/test-my_t_test.R
@@ -0,0 +1,11 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("error occurs with invalid alternative",{
+  expect_error(my_t_test(rnorm(40), "one.sided", 0))
+})
+
+test_that("length of output is 4",{
+  expect_equal(length(my_t_test(rnorm(40), "two.sided", 0)),4)
+})
